7b509344599a1829438d8079ca5160ed42970fec
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-01-15 18:17:28 +0500

    empty commit

--- src/components/Home/Hero/Slider/index.tsx
+++ src/components/Home/Hero/Slider/index.tsx
@@ -9,7 +9,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 //   Pagination,
 // } from "swiper/modules";
 import "swiper/css";
-
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
