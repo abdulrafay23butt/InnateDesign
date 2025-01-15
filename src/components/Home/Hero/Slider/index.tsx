bc27fae8eceb4f2cd5c21b4df61bf677835d5934
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-01-15 18:39:48 +0500

    add pagination

--- src/components/Home/Hero/Slider/index.tsx
+++ src/components/Home/Hero/Slider/index.tsx
@@ -1,17 +1,17 @@
 "use client";
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-// import {
-//   Keyboard,
-//   Navigation,
-//   Scrollbar,
-//   Autoplay,
-//   Pagination,
-// } from "swiper/modules";
+import {
+  Keyboard,
+  Navigation,
+  Scrollbar,
+  Autoplay,
+  Pagination,
+} from "swiper/modules";
 import "swiper/css";
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
 import "./slider.css";
 import Slide1 from "../Content/slide1";
 import Slide2 from "../Content/slide2";
@@ -20,27 +20,39 @@ import Slide4 from "../Content/slide4";
 
 function HomeSlider() {
   const slides = [
-    { component: <Slide1 />, delay: 36000 },
-    { component: <Slide2 />, delay: 26000 },
-    { component: <Slide3 />, delay: 42000 },
-    { component: <Slide4 />, delay: 14000 },
+    { component: <Slide1 />, delay: 36000 }, 
+    { component: <Slide2 />, delay: 26000 }, 
+    { component: <Slide3 />, delay: 42000 }, 
+    { component: <Slide4 />, delay: 14000 }, 
   ];
 
   return (
     <div className="w-full overflow-hidden">
       <div className="w-full overflow-hidden home-slider-parent">
         <Swiper
+          slidesPerView={1}
+          speed={2000}
+          autoplay={{
+            delay: 5000, // Default delay
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          navigation={{
+            nextEl: ".custom-next",
+            prevEl: ".custom-prev",
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Keyboard, Navigation, Scrollbar, Autoplay, Pagination]}
+          className="mySwiper"
           onSlideChange={(swiper) => {
-            const currentSlideIndex = swiper.realIndex;
-            const videoElements = document.querySelectorAll('video');
-            videoElements.forEach((video, index) => {
-              if (index === currentSlideIndex) {
-                video.play();
-              } else {
-                video.pause();
-                video.currentTime = 0; // Reset video when not active
-              }
-            });
+            // Ensure autoplay exists and is of type AutoplayOptions
+            if (swiper.params.autoplay && typeof swiper.params.autoplay === "object") {
+              const currentSlide = swiper.realIndex;
+              swiper.params.autoplay.delay = slides[currentSlide]?.delay || 5000; // Fallback to default delay
+              swiper.autoplay.start(); // Restart autoplay to apply the new delay
+            }
           }}
         >
           {slides.map((slide, index) => (
@@ -49,8 +61,6 @@ function HomeSlider() {
             </SwiperSlide>
           ))}
         </Swiper>
-
-
       </div>
     </div>
   );
